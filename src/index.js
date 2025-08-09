104567534e696212654483439302d0f33aff2e4b
Yuriy <162017185+matb22@users.noreply.github.com>
2025-08-09 23:02:41 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from './App'
+import './css/main.css'
+
+createRoot(document.getElementById('main-app')).render(<App></App>)
